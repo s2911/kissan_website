a6996212a00cf7bac72f836f91f90431c460db71
s2911 <146133503+s2911@users.noreply.github.com>
2024-08-29 01:35:06 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <div>
+        <h1>K-I-S-S-A-N</h1>
+        <p>@all rights reserved</p>
+    </div>
+
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://instagram.com/i.am.sahil__" target={"blank"}>
+            Instagram
+            </a>
+            <a href="https://github.com/s2911" target={"blank"}>
+            GitHub
+            </a>
+        </div>
+    </div>
+  </footer>
+};
+
+export default Footer;
---
+++ src/components/Services.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from "../assets/3.jpg";
+import img2 from "../assets/2.jpg";
+
+const Services = () => {
+  return <div>
+    <Carousel 
+    infiniteLoop 
+    autoPlay 
+    showStatus={false} 
+    showArrows={false}
+    showThumbs={false}
+    interval={1000}>
+        <div>
+            <img src={img1} alt="Item1" />
+            <p className="legend">K-I-S-S-A-N</p>
+        </div>
+
+        <div>
+            <img src={img2} alt="Item2" />
+            <p className="legend">Peer-to-peer Support</p>
+        </div>
+    </Carousel>
+  </div>
+};
+
+export default Services;
